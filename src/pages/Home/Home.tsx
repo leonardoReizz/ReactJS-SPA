04e6ef6a644bf0526cad8c13d13e126723eccd67
Leonardo Reis <amarantee.reis@gmail.com>
2022-11-30 16:02:39 -0300

    first commit

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,28 @@
+import { CountdownContainer, FormContainer, HomeContainer, Separator } from './style'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input id="task" />
+
+          <label htmlFor="">durante</label>
+          <input type="number" id="minutesAmount" />
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <button type="submit">Começar</button>
+      </form>
+    </HomeContainer>
+  )
+}
